0ebe93a2c8b423e709ab4a41b39b8092b612a9c3
apolgesek <iceman129@gmail.com>
2025-12-14 17:52:24 +0100

    feat: loading spinner

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,19 +1,30 @@
-import { Component, inject } from '@angular/core';
-import { RouterLink, RouterOutlet } from '@angular/router';
+import { Component, inject, OnInit, signal } from '@angular/core';
+import { NavigationEnd, NavigationStart, Router, RouterLink, RouterOutlet } from '@angular/router';
 import { NzIconModule } from 'ng-zorro-antd/icon';
 import { NzLayoutModule } from 'ng-zorro-antd/layout';
 import { NzMenuModule } from 'ng-zorro-antd/menu';
+import { NzSpinModule } from 'ng-zorro-antd/spin';
 import { ConfigService } from './core';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterLink, RouterOutlet, NzIconModule, NzLayoutModule, NzMenuModule],
+  imports: [RouterLink, RouterOutlet, NzIconModule, NzLayoutModule, NzMenuModule, NzSpinModule],
   templateUrl: './app.html',
   styleUrl: './app.scss',
 })
-export class App {
+export class App implements OnInit {
   private readonly configService = inject(ConfigService);
+  private readonly router = inject(Router);
   appVersion = this.configService.version;
-}
+  isLoading = signal(false);
 
-// refresh 1
+  ngOnInit(): void {
+    this.router.events.subscribe((e) => {
+      if (e instanceof NavigationStart) {
+        this.isLoading.set(true);
+      } else if (e instanceof NavigationEnd) {
+        this.isLoading.set(false);
+      }
+    });
+  }
+}
