d08c9d78e7294718f98e12e54e6809efa3ec9995
apolgesek <iceman129@gmail.com>
2025-12-14 13:15:58 +0100

    fix: get policies

--- src/app/modules/policies/policy-strategy.ts
+++ src/app/modules/policies/policy-strategy.ts
@@ -2,6 +2,7 @@ import { Observable } from 'rxjs';
 import { AddPolicyRequest } from './models';
 import { PolicyResponse } from './services/policies-http';
 
-export abstract class AddPolicyStrategy {
+export abstract class PolicyStrategy {
   abstract addPolicy(request: AddPolicyRequest): Observable<PolicyResponse[]>;
+  abstract getPolicies(): Observable<PolicyResponse[]>;
 }
--- src/app/modules/policies/sso-policies.routes.ts
+++ src/app/modules/policies/sso-policies.routes.ts
@@ -3,13 +3,18 @@ import { Policies } from './policies';
 import { PoliciesHttp } from './services/policies-http';
 import { inject, Injectable } from '@angular/core';
 import { AddPolicyRequest } from './models';
-import { AddPolicyStrategy } from './add-policy-strategy';
+import { PolicyStrategy } from './policy-strategy';
 
 @Injectable()
-export class AddSSOPolicyStrategy extends AddPolicyStrategy {
+export class SSOPolicyStrategy extends PolicyStrategy {
   private readonly policiesHttp = inject(PoliciesHttp);
+
   addPolicy(request: AddPolicyRequest) {
     return this.policiesHttp.addSSOPolicy(request);
+  }
+
+  getPolicies() {
+    return this.policiesHttp.getSSOPolicies();
   }
 }
 
@@ -17,7 +22,7 @@ export const POLICIES_ROUTES: Routes = [
   {
     path: '',
     component: Policies,
-    providers: [{ provide: AddPolicyStrategy, useClass: AddSSOPolicyStrategy }],
+    providers: [{ provide: PolicyStrategy, useClass: SSOPolicyStrategy }],
     resolve: {
       list: () => {
         const policiesHttp = inject(PoliciesHttp);
